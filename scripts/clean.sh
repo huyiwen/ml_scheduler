046021c3b58fd213829e6ba7cd6ee3825afb512d
huyiwen <1020030101@qq.com>
2024-06-26 16:34:52 +0800

    setting up template

--- scripts/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-poetry run isort ml_scheduler/ tests/
-poetry run black ml_scheduler/ tests/
--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-./scripts/lint.sh
-poetry run pytest -s --cov=ml_scheduler/ --cov=tests --cov-report=term-missing ${@-} --cov-report html
